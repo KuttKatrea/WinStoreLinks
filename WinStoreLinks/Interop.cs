1e5192d3d4dd04ed3125559423d224a791294e22
Alan Reyes <kutt@katrea.net>
2023-10-06 23:20:21 -0600

    First working version

---
+++ WinStoreLinks/Interop.cs
@@ -0,0 +1,39 @@
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Net.Katrea.WinStoreLinks;
+
+// https://csharp.hotexamples.com/site/file?hash=0x3b8f17294044441359d546499385ffb55c0e7b132b2d655f05aa5a20c0ecc341&fullName=Windows+Shortcut+Resolver.cs&project=stavrossk%2FWindows_Game_Metadata_Retriever_for_.NET
+// https://csharp.hotexamples.com/examples/-/ShellLink/-/php-shelllink-class-examples.html
+
+[ComImport]
+[Guid("00021401-0000-0000-C000-000000000046")]
+internal class ShellLink
+{
+}
+
+[ComImport]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+[Guid("000214F9-0000-0000-C000-000000000046")]
+internal interface IShellLink
+{
+    void GetPath([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszFile, int cchMaxPath, out IntPtr pfd, int fFlags);
+    void GetIDList(out IntPtr ppidl);
+    void SetIDList(IntPtr pidl);
+    void GetDescription([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszName, int cchMaxName);
+    void SetDescription([MarshalAs(UnmanagedType.LPWStr)] string pszName);
+    void GetWorkingDirectory([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszDir, int cchMaxPath);
+    void SetWorkingDirectory([MarshalAs(UnmanagedType.LPWStr)] string pszDir);
+    void GetArguments([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszArgs, int cchMaxPath);
+    void SetArguments([MarshalAs(UnmanagedType.LPWStr)] string pszArgs);
+    void GetHotkey(out short pwHotkey);
+    void SetHotkey(short wHotkey);
+    void GetShowCmd(out int piShowCmd);
+    void SetShowCmd(int iShowCmd);
+    void GetIconLocation([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszIconPath, int cchIconPath, out int piIcon);
+    void SetIconLocation([MarshalAs(UnmanagedType.LPWStr)] string pszIconPath, int iIcon);
+    void SetRelativePath([MarshalAs(UnmanagedType.LPWStr)] string pszPathRel, int dwReserved);
+    void Resolve(IntPtr hwnd, int fFlags);
+    void SetPath([MarshalAs(UnmanagedType.LPWStr)] string pszFile);
+}
+
---
+++ WinStoreLinks/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices.ComTypes;
+using System.Xml.Linq;
+using Windows.ApplicationModel;
+using Windows.Management.Deployment;
+using Windows.Storage;
+using Windows.System;
+using Net.Katrea.WinStoreLinks;
+using Ookii.CommandLine;
+
+void CreateLinks(CommandLineOptions commandLineOptions)
+{
+    var packageManager = new PackageManager();
+
+    var packages =
+        (IEnumerable<Package>)packageManager.FindPackagesForUser("");
+       
+    foreach (var packageItem in packages)
+    {
+        Console.WriteLine("----");
+        Console.WriteLine(packageItem.DisplayName);
+        Console.WriteLine(packageItem.InstalledLocation.Path);
+
+        var manifestFile = packageItem.InstalledLocation.GetFileAsync("AppxManifest.xml").GetAwaiter().GetResult();
+        var manifestXml = FileIO.ReadTextAsync(manifestFile).GetAwaiter().GetResult();
+
+        var doc = XDocument.Parse(manifestXml);
+        XNamespace packageNamespace = "http://schemas.microsoft.com/appx/manifest/foundation/windows10";
+        XNamespace uapNamespace = "http://schemas.microsoft.com/appx/manifest/uap/windows10";
+
+        var hidden = new HashSet<string>();
+
+        foreach (var it in doc.Descendants(packageNamespace + "Application"))
+        {
+            var appId = it.Attribute("Id")?.Value ?? "";
+            var ve = it.Descendants(uapNamespace + "VisualElements").FirstOrDefault();
+            if (ve?.Attribute("AppListEntry")?.Value == "none")
+            {
+                hidden.Add(appId);
+            }
+        }
+            
+        Console.WriteLine($"Hidden: {string.Join(", ", hidden)}");
+            
+        var packageApps = AppDiagnosticInfo.RequestInfoForPackageAsync(packageItem.Id.FamilyName).GetAwaiter().GetResult();
+        foreach (var k in packageApps)
+        {
+            Console.WriteLine($"{k.AppInfo.DisplayInfo.DisplayName} ({k.AppInfo.Id})");
+            if (hidden.Contains(k.AppInfo.Id))
+            {
+                Console.WriteLine("HIDDEN");
+                continue;
+            } 
+            CreateLink(commandLineOptions.DirectoryPath, packageItem.Id.FamilyName, k.AppInfo.Id, k.AppInfo.DisplayInfo.DisplayName, k.AppInfo.DisplayInfo.Description);    
+        }
+    }
+}
+
+void CreateLink(string linkFolder, string appFamily, string appId, string appName, string appDescription)
+{
+    IShellLink link = (IShellLink)new ShellLink();
+
+    // setup shortcut information
+    link.SetDescription(appDescription);
+    link.SetPath($@"shell:appsFolder\{appFamily}!{appId}");
+
+    // save it
+    ((IPersistFile)link).Save(Path.Combine(linkFolder, $"{appName}.lnk"), false);
+}
+
+var parser = new CommandLineParser(typeof(CommandLineOptions));
+CommandLineOptions arguments;
+try
+{
+    arguments = (CommandLineOptions)parser.Parse(args)!;
+}
+catch (CommandLineArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+    parser.WriteUsage();
+    return;
+}
+
+CreateLinks(arguments);
+
+public class CommandLineOptions
+{
+    [CommandLineArgument(Position = 0, IsRequired = true)]
+    [Description("Folder to put the links")]
+    public string? DirectoryPath { get; set; }
+}
